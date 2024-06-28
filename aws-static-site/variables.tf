c596845e6551a046f241177b0867bcb1ee352243
Chase <chase@slept.dev>
2024-06-28 13:16:44 -0500

    added variables for cloudflare provider

--- aws-static-site/variables.tf
+++ aws-static-site/variables.tf
@@ -3,7 +3,18 @@ variable "aws_region" {
   description = "The AWS region of the bucket"
 }
 
+variable "sub_domain" {
+  type = string
+  description = "The subdomain to set for our CNAME record in Cloudflare"
+  default = tf-test
+}
+
 variable "site_domain" {
   type = string
   description = "The Cloudflare domain name for setting CNAME record"
+}
+
+variable "cloudflare_api_token" {
+  type = string
+  description = "The Cloudflare api token to make DNS changes"
 }

67e547ad3508a8269dd987c232a961031714af06
Chase <chase@slept.dev>
2024-06-28 12:45:27 -0500

    added website files; add site_domain variable

--- aws-static-site/variables.tf
+++ aws-static-site/variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   type = string
   description = "The AWS region of the bucket"
 }
+
+variable "site_domain" {
+  type = string
+  description = "The Cloudflare domain name for setting CNAME record"
+}
